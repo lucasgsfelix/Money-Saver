5969a5c41165c171e2148db0022cad8dd09c2ca2
Lucasgsfelix <lucasgsfelix@gmail.com>
2018-01-08 20:47:37 -0200

    Learning JS / First steps

---
+++ moneySaver.js
@@ -0,0 +1,27 @@
+
+function parser(){
+
+    //this function will be responsible for receive the page and get the price of the product
+    readTextFile("tLotR")
+
+
+
+}
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
